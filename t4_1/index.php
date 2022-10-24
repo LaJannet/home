fac14f5aef4bbe9991b986881c4a580deec8f30a
LaJannet <lajannetwork@gmail.com>
2022-10-25 02:04:59 +0300

    initial commit

--- t4_1/index.php
+++ t4_1/index.php
@@ -1,6 +1,7 @@
 <?php
 print "<table style =\"border:solid;\">";
-for ($i=1; $i < 8; $i++) { 
+$n = rand(1, 8);
+for ($i=1; $i < $n; $i++) { 
     if ($i%2!=0) {
         print "<tr><td style =\"border:solid; background-color:orange; padding: 0 10px;\">$i</td> <td  style =\"border:solid; background-color:black;\"> <img style =\"width:100px; padding: 10px\" src= \"images/img_{$i}.jpg\"><p style =\"color:lightblue; margin: auto; text-align: center;\">img_{$i}.jpg</p></td></tr>";
     }
